95fa8eb3dfc77ff8dfed3989d28a8de39f77b791
Ritika <bhardwajritika11@gmail.com>
2026-02-10 18:23:41 +0530

    Jornal Detail screen, Jornal Entry and Map Screen done with mapkit

---
+++ JournalStory/Journal List Screen/Model/JournalEntry.swift
@@ -0,0 +1,8 @@
+//
+//  JournalEntry.swift
+//  JournalStory
+//
+//  Created by Tarun Sharma on 09/02/26.
+//
+
+import Foundation
---
+++ JournalStory/Journal List Screen/View/JournalListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  JournalListTableViewCell.swift
+//  JournalStory
+//
+//  Created by Tarun Sharma on 09/02/26.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
